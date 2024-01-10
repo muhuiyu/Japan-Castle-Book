f97b48475088386df4585215556d124e1a9ba902
muhuiyu <muhuiyu.grace@gmail.com>
2024-01-10 23:38:21 +0800

    Initial Commit

---
+++ JapanCastleBook/JapanCastleBook.h
@@ -0,0 +1,18 @@
+//
+//  JapanCastleBook.h
+//  JapanCastleBook
+//
+//  Created by Mu Yu on 1/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JapanCastleBook.
+FOUNDATION_EXPORT double JapanCastleBookVersionNumber;
+
+//! Project version string for JapanCastleBook.
+FOUNDATION_EXPORT const unsigned char JapanCastleBookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JapanCastleBook/PublicHeader.h>
+
+
