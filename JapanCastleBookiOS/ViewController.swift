25ad5ef7af3929a7681459cc096331a6486bf789
muhuiyu <muhuiyu.grace@gmail.com>
2024-01-11 06:51:10 +0800

    Add iOS target

---
+++ JapanCastleBookiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JapanCastleBookiOS
+//
+//  Created by Mu Yu on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
