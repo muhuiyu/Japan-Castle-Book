46354269d127dde5de59485e54658867b26f9482
muhuiyu <muhuiyu.grace@gmail.com>
2024-01-11 06:48:57 +0800

    Move memory leak tracking method to its own file

--- JapanCastleBookTests/LocalCastleServiceIntegrationTests.swift
+++ JapanCastleBookTests/LocalCastleServiceIntegrationTests.swift
@@ -154,11 +154,3 @@ extension LocalCastleServiceIntegrationTests {
         ][index]
     }
 }
-
-extension XCTestCase {
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-}
---
+++ JapanCastleBookTests/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  JapanCastleBookTests
+//
+//  Created by Mu Yu on 1/11/24.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
