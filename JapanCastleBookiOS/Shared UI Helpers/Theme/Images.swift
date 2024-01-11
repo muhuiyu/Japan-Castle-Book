898b4f7cfb8ca398fc553ff194311e862e85f43e
muhuiyu <muhuiyu.grace@gmail.com>
2024-01-11 08:31:29 +0800

    Add castle and stamp images

---
+++ JapanCastleBookiOS/Shared UI Helpers/Theme/Images.swift
@@ -0,0 +1,13 @@
+//
+//  Images.swift
+//  JapanCastleBookiOS
+//
+//  Created by Mu Yu on 1/11/24.
+//
+
+import UIKit
+
+final class Images {
+    static let castlePlaceholder = UIImage(named: "castle")
+    static let castleDoneStamp = UIImage(named: "done-stamp")
+}
